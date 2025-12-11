1de66d6f62ff81d6fabfa6e0149ced50418103d7
Gabriel <gabriel.sanches170@gmail.com>
2025-12-11 17:42:27 -0300

    feat: :recycle: changes main page to [lang] folder

---
+++ app/[lang]/page.tsx
@@ -0,0 +1,72 @@
+import Header from '@/components/sections/Header';
+import { query } from '@/lib/qs';
+import { client } from '@/services/strapi-client';
+import { StrapiApiResponse } from '@/types/strapi';
+
+type GetDataResponse = Promise<StrapiApiResponse['data'] | null>;
+
+async function getData(lang: string): GetDataResponse {
+  try {
+    const response = await client.fetch(`content?${query(lang)}`);
+    const { data }: StrapiApiResponse = await response.json();
+
+    return data;
+  } catch (error: any) {
+    if (error instanceof Error) {
+      console.log('Strapi API Error: ', error.message);
+    } else {
+      console.error('Strapi API Error: ', error);
+    }
+
+    return null;
+  }
+}
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ lang: string }>;
+}) {
+  const { lang } = await params;
+  const data = await getData(lang);
+
+  // console.log('🚀 ~ Page ~ data:', data);
+
+  if (!data) return <></>;
+
+  return (
+    <body className={`bg-background dark:bg-background`}>
+      <Header data={data?.header_section} />
+      {/* <main className='w-full px-5'>
+        <Hero>
+          <Hero.Header />
+          <Hero.Buttons />
+          <Hero.Divider />
+          <Hero.SocialMedia />
+        </Hero>
+
+        <About>
+          <About.Description />
+          <About.Image />
+        </About>
+
+        <Projects>
+          <Projects.Header />
+          <Projects.List />
+        </Projects>
+
+        <Experience>
+          <Experience.Header />
+          <Experience.Timeline />
+        </Experience>
+
+        <Skill>
+          <Skill.Header />
+          <Skill.List />
+        </Skill>
+
+        <Footer />
+      </main> */}
+    </body>
+  );
+}
