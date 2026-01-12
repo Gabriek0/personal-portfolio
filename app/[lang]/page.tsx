63a8ba753cbfdc8cde49519169c1c73d40233741
Gabriel <gabriel.sanches170@gmail.com>
2026-01-12 15:46:04 -0300

    feat: :art: add custom delay and page structure

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,17 +1,26 @@
+import About from '@/components/sections/About';
+import Experience from '@/components/sections/Experience';
+import Footer from '@/components/sections/Footer';
 import Header from '@/components/sections/Header';
+import Hero from '@/components/sections/Hero';
+import Projects from '@/components/sections/Projects';
+import Skill from '@/components/sections/Skill';
 import { query } from '@/lib/qs';
 import { client } from '@/services/strapi-client';
 import { StrapiApiResponse } from '@/types/strapi';
 import { notFound } from 'next/navigation';
-import React from 'react';
 
 type GetDataResponse = Promise<StrapiApiResponse['data'] | null>;
+
+const delay = async (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
 
 async function getData(lang: string): GetDataResponse {
   try {
     const response = await client.fetch(`content?${query(lang)}`);
     const { data }: StrapiApiResponse = await response.json();
 
+    await delay(2000);
     return data;
   } catch (error: any) {
     if (error instanceof Error) {
@@ -37,39 +46,14 @@ export default async function Page({
   }
 
   return (
-    <React.Fragment>
-      <Header data={data?.header_section} />
-
-      {/* <main className='w-full px-5'>
-        <Hero>
-          <Hero.Header />
-          <Hero.Buttons />
-          <Hero.Divider />
-          <Hero.SocialMedia />
-        </Hero>
-
-        <About>
-          <About.Description />
-          <About.Image />
-        </About>
-
-        <Projects>
-          <Projects.Header />
-          <Projects.List />
-        </Projects>
-
-        <Experience>
-          <Experience.Header />
-          <Experience.Timeline />
-        </Experience>
-
-        <Skill>
-          <Skill.Header />
-          <Skill.List />
-        </Skill>
-
-        <Footer />
-      </main> */}
-    </React.Fragment>
+    <main className='w-full px-5'>
+      <Header data={data.header_section} />
+      <Hero data={data.hero_section} />
+      <About data={data.about_section} />
+      <Projects data={data.project_section} />
+      <Experience data={data.experience_section} />
+      <Skill />
+      <Footer />
+    </main>
   );
 }
