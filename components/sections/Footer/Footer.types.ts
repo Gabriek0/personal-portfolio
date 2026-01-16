827aca2b5bb43c7f565b21cd46418f8df0941f16
Gabriel <gabriel.sanches170@gmail.com>
2026-01-15 22:14:41 -0300

    feat: :sparkles: populate Footer section

---
+++ components/sections/Footer/Footer.types.ts
@@ -0,0 +1,5 @@
+import { FooterSection } from '@/types/strapi';
+
+export interface FooterProps {
+  data: FooterSection;
+}
--- components/sections/Footer/index.tsx
+++ components/sections/Footer/index.tsx
@@ -1,20 +1,31 @@
+'use client';
+
 import { Button } from '@/components/ui/Button';
 import Divider from '@/components/ui/Divider';
 import { CircleArrowUp } from 'lucide-react';
+import { FooterProps } from './Footer.types';
 
-export default function Footer() {
+export default function Footer({ data }: FooterProps) {
   return (
     <footer className='mt-0 mb-6 mx-auto min-w-80 max-w-80 md:max-w-155 md:min-w-155 lg:min-w-192 md:mb-16'>
       <Divider />
 
       <div className='w-full flex justify-between '>
-        <Button className='bg-transparent hidden underline text-text-important md:flex md:items-center md:gap-2'>
-          Voltar para o topo
+        <Button
+          onClick={() => {
+            window.scrollTo({
+              top: 0,
+              behavior: 'smooth',
+            });
+          }}
+          className='bg-transparent hidden underline text-text-important md:flex md:items-center md:gap-2'
+        >
+          {data.footer_back_to_top_button.button_text}
           <CircleArrowUp className='h-4.5 w-4.5 ' />
         </Button>
 
         <span className='text-sm font-normal text-text-important md:text-base'>
-          Gabriel Henrique © 2025 Todos direitos reservados
+          {data.footer_copyright}
         </span>
       </div>
     </footer>
