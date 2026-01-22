c430b8387c0a0db4454d02a331ff5499d49c3802
Gabriel <gabriel.sanches170@gmail.com>
2026-01-22 19:26:31 -0300

    fix: :bug: HeaderNavbar component declaration

--- components/sections/Header/index.tsx
+++ components/sections/Header/index.tsx
@@ -8,7 +8,8 @@ function Header({ data }: HeaderProps) {
   return (
     <header className='z-10 w-full mx-auto flex gap-4 items-center justify-between py-6.5 px-5 xl:py-8 xl:px-28 md:sticky md:top-0'>
       <HeaderLogo />
-l      <HeaderThemeSwitcher />
+      <HeaderNavbar data={data} />
+      <HeaderThemeSwitcher />
       <HeaderMenuToggle data={data} />
     </header>
   );
