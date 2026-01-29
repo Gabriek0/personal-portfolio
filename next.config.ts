9db106f8d92e21b48353623aa4cf6a1239e98768
Gabriel <gabriel.sanches170@gmail.com>
2026-01-28 23:09:12 -0300

    fix(next.config.ts): :wrench: add image remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,8 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '1337',
-        pathname: '/uploads/**',
+        protocol: 'https',
+        hostname: process.env.STRAPI_API_MEDIA_HOSTNAME,
       },
     ],
   },
