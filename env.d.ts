0ffd949699ea0d8873c5441d7b396f6233fc4b75
Gabriel <gabriel.sanches170@gmail.com>
2026-01-28 23:08:23 -0300

    feat: :label: update environment variable types

--- env.d.ts
+++ env.d.ts
@@ -2,6 +2,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     STRAPI_API_URL: string;
     STRAPI_API_TOKEN: string;
+    STRAPI_API_MEDIA_HOSTNAME: string;
     NEXT_PUBLIC_STRAPI_URL: string;
   }
 }
