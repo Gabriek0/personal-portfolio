96c1e5e287a89b8c71ad2fcf4f3b009073229ac7
Gabriel <gabriel.sanches170@gmail.com>
2026-03-02 19:12:37 -0300

    chore: :label: add STRAPI_WEBHOOK_TOKEN type

--- env.d.ts
+++ env.d.ts
@@ -2,7 +2,8 @@ declare namespace NodeJS {
   interface ProcessEnv {
     STRAPI_API_URL: string;
     STRAPI_API_TOKEN: string;
-    STRAPI_API_MEDIA_HOSTNAME: string;
+    STRAPI_WEBHOOK_TOKEN: string;
     NEXT_PUBLIC_STRAPI_URL: string;
+    STRAPI_API_MEDIA_HOSTNAME: string;
   }
 }
