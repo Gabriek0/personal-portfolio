9cb67a6871fb027f38194b632d3ba44bb8fbf0fe
Gabriel <gabriel.sanches170@gmail.com>
2026-03-12 13:12:06 -0300

    feat: get blog data from strapi and md files

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,12 +1,15 @@
 import About from '@/src/features/about';
+import Blog from '@/src/features/blog';
+import { getAllPosts } from '@/src/features/blog/server/db/posts';
 import Experience from '@/src/features/experience';
 import Footer from '@/src/features/footer';
 import Header from '@/src/features/header';
 import Hero from '@/src/features/hero';
 import Projects from '@/src/features/projects';
 import Skill from '@/src/features/skills';
-import { query } from '@/src/lib/strapi/query';
-import { StrapiApiResponse } from '@/src/types/strapi';
+import { query } from '@/src/shared/lib/strapi/query';
+import { Posts as IPosts } from '@/src/shared/types/posts';
+import { StrapiApiResponse } from '@/src/shared/types/strapi';
 import { notFound } from 'next/navigation';
 
 type GetDataResponse = Promise<StrapiApiResponse['data'] | null>;
@@ -52,6 +55,7 @@ export default async function Page({
   const { lang } = await params;
 
   const data = await getPortfolioData(lang);
+  const posts = JSON.parse(JSON.stringify(await getAllPosts(3))) as IPosts;
 
   if (!data) {
     notFound();
@@ -62,6 +66,7 @@ export default async function Page({
     hero_section,
     about_section,
     project_section,
+    blog_section,
     experience_section,
     skill_section,
     footer_section,
@@ -73,6 +78,7 @@ export default async function Page({
       <Hero data={hero_section} />
       <About data={about_section} />
       <Projects data={project_section} />
+      <Blog data={{ ...blog_section, posts_list: posts }} />
       <Experience data={experience_section} />
       <Skill data={skill_section} />
       <Footer data={footer_section} />
