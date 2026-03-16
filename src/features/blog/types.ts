742831279a6a71d47761fb16dc61bc406b60da63
Gabriel <gabriel.sanches170@gmail.com>
2026-03-16 11:13:13 -0300

    feat: create types to blog feature

--- src/features/blog/types.ts
+++ src/features/blog/types.ts
@@ -1,5 +1,6 @@
-import { BlogSection } from "@/src/shared/types/strapi";
+import { blogDict } from "./lib/dictionaries";
+import { BlogSection } from '@/src/shared/types/strapi';
 
-export interface BlogProps {
-  data: BlogSection;
-}
+export interface BlogProps { data: BlogSection }
+
+export type BlogDictKey = keyof (typeof blogDict)['en'];
