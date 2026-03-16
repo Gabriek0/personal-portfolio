4d0004cfe3cba404ca9c28c7efddb9f972e78399
Gabriel <gabriel.sanches170@gmail.com>
2026-03-16 11:16:06 -0300

    feat: add next-mdx-remote to transpilePackages and add image remotePatterns local

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,23 @@
+import createMDX from '@next/mdx';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: process.env.STRAPI_API_MEDIA_HOSTNAME,
-      },
       // {
-      //   protocol: 'http',
-      //   hostname: 'localhost',
-      //   port: '1337',
-      //   pathname: '/uploads/**',
+      //   protocol: 'https',
+      //   hostname: process.env.STRAPI_API_MEDIA_HOSTNAME,
       // },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '1337',
+        pathname: '/uploads/**',
+      },
     ],
   },
+  transpilePackages: ['next-mdx-remote'],
   turbopack: {
     rules: {
       '*.svg': {
@@ -25,4 +28,8 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  extension: /\.(md|mdx)$/,
+});
+
+export default withMDX(nextConfig);
