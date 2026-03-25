93412e41618a1875f24ffb442a25b2235245e812
Gabriel <gabriel.sanches170@gmail.com>
2026-03-24 23:16:02 -0300

    feat: add typings to PostBySlugResult

--- src/features/blog/types.ts
+++ src/features/blog/types.ts
@@ -1,6 +1,12 @@
-import { blogDict } from "./lib/dictionaries";
 import { BlogSection } from '@/src/shared/types/strapi';
+import { blogDict } from "./lib/dictionaries";
 
 export interface BlogProps { data: BlogSection }
 
 export type BlogDictKey = keyof (typeof blogDict)['en'];
+
+export type PostBySlugResult = {
+ data: Record<string, unknown>;
+ content: string;
+ isTranslationAvailable: boolean;
+};
