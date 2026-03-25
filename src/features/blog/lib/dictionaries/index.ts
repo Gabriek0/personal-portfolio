8bb3e3599ba07225c063833ca99249e8c1ee4a19
Gabriel <gabriel.sanches170@gmail.com>
2026-03-24 23:13:02 -0300

    feat: add notTranslated property

--- src/features/blog/lib/dictionaries/index.ts
+++ src/features/blog/lib/dictionaries/index.ts
@@ -3,9 +3,21 @@ import { Lang, TranslationStructure } from '@/src/shared/types';
 import { BlogDictKey } from '../../types';
 
 export const blogDict = {
-  en: { back: 'Back', readingTime: 'min read' },
-  es: { back: 'Volver', readingTime: 'min de lectura' },
-  'pt-br': { back: 'Voltar', readingTime: 'min de leitura' },
+  en: {
+    back: 'Back',
+    readingTime: 'min read',
+    notTranslated: "This post isn't available in your selected language yet. Showing the English version.",
+  },
+  es: {
+    back: 'Volver',
+    readingTime: 'min de lectura',
+    notTranslated: 'Esta publicación aún no está disponible en español. Se muestra la versión en inglés.',
+  },
+  'pt-br': {
+    back: 'Voltar',
+    readingTime: 'min de leitura',
+    notTranslated: 'Esta publicação ainda não está disponível em português. Exibindo a versão em inglês.',
+  },
 } satisfies TranslationStructure;
 
 export const t = (key: BlogDictKey, lang: string) => {
